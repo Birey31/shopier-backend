6ee49945ed0fc85531b3a40bcf18f99a73507a74
Birey31 <100028750+Birey31@users.noreply.github.com>
2026-01-09 22:41:01 +0300

    Update get-paytr-token.js

--- api/get-paytr-token.js
+++ api/get-paytr-token.js
@@ -1,3 +1,12 @@
+res.setHeader('Access-Control-Allow-Credentials', true);
+res.setHeader('Access-Control-Allow-Origin', '*'); // Veya sadece kendi site linkin
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+}
 const crypto = require('crypto');
 const axios = require('axios');
 
